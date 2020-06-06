0ad52848d36c49680c8c5914cfbeb3af02b33ee8
p1003 <pitrus1003@gmail.com>
2020-06-06 17:44:02 +0200

    Database working

--- src/main/java/PitrusMain.java
+++ src/main/java/PitrusMain.java
@@ -8,19 +8,19 @@ public class PitrusMain {
     public static void main(String[] args) {
         DatabaseProvider db = DatabaseProvider.getInstance();
 
-        db.addUser(new User("Papaj","Karol","papa@gmal","kaslo"));
+//        db.addUser(new User("Papaj","Karol","papa@gmal","kaslo"));
 
         User user = db.getUserByName("Papaj","Karol");
 
-        Recipe initRecipe = new Recipe(user,"Kawa");
-        initRecipe.setDescription("Pychota");
-        initRecipe.setIngredients("Ziemniaki\n");
-
-        db.addRecipe(initRecipe);
+//        Recipe initRecipe = new Recipe(user,"Kawa");
+//        initRecipe.setDescription("Pychota");
+//        initRecipe.setIngredients("Ziemniaki\n");
+//
+//        db.addRecipe(initRecipe);
 
         Recipe recipe = db.getRecipeByTitle("Kawa");
 
-        db.addRating(new Rating(user,recipe,1));
+//        db.addRating(new Rating(user,recipe,1));
 
         System.out.println("Ratings");
         for(Rating rating : recipe.getRatings()) {
